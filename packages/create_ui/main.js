ff3f7a78cc61a52def0f3cf1224f83052a0911b0
zhangxx <zxx178239@163.com>
2019-08-05 21:11:58 +0800

    package学习和添加

---
+++ packages/create_ui/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = {
+    load() {
+
+    },
+
+    unload() {
+
+    },
+
+    messages: {
+        open() {
+            Editor.log("create ui");
+            Editor.Panel.open("create_ui");
+        }
+    }
+}
